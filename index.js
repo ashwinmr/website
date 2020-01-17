ae402590daba289e5705cdeb9a7089fb8d5af553
ashwinmr <iashwinrao@gmail.com>
2020-01-17 22:18:42 +0530

    add assets and css

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
+// Allow assets to be used by website
+app.use(express.static(path.join(__dirname, 'assets')));
+
 app.get('/', (req, res) => {
-    res.send('Hey, I\'m a Node.js app!')
+    res.sendFile(path.join(__dirname, 'src', 'index.html'))
 })
 
 app.listen(3000, () => {
