b89cc7ef6ab0058ecdb3370d36d6689339984292
ashwinmr <iashwinrao@gmail.com>
2020-01-21 14:44:39 +0530

    reorganize and add pages

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const express = require('express')
+
+
 const app = express()
 
-// Allow assets to be used by website
-app.use(express.static(path.join(__dirname, 'assets')));
+// Allow public to be used by website
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'))
 })
 
 app.listen(3000, () => {
