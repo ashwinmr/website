c9f097b503fa2aedc78f386db5b66c81ed72614a
ashwinmr <iashwinrao@gmail.com>
2020-02-04 19:54:17 +0530

    use pug as template engine

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const app = express()
 // Allow public to be used by website
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set views folder
+app.set('views', path.join(__dirname, 'views'))
+
+// Set view engine
+app.set('view engine', 'pug')
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'))
+// })
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'))
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(3000, () => {
